39891b6016d19d21ea818e46df1d6c4fb3275023
Christoph Honal <christoph.honal@web.de>
2022-06-22 12:04:34 +0200

    add vscode and nixos support

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShell.x86_64-linux =
+        pkgs.mkShell {
+          shellHook = ''
+          '';
+
+          buildInputs = with pkgs; [
+            gdb
+            gcc
+            cmake
+            libusb1
+            libusb-compat-0_1
+            pkg-config
+            clang-tools
+          ];
+        };
+    };
+}
